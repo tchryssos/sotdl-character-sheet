79f9e1cdb7adb4953868858fcd9a9be3fe4442ec
Troy Chryssos <troychryssos@gmail.com>
2021-09-26 12:14:35 -0400

    cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import styled from '@emotion/styled';
-import startCase from 'lodash.startcase';
 
 import { FlexBox } from '~/components/box/FlexBox';
 import { GridBox } from '~/components/box/GridBox';
@@ -10,22 +9,16 @@ import { ArmorInput } from '~/components/form/gameInputs/ArmorInput';
 import { AttributeInput } from '~/components/form/gameInputs/AttributeInput';
 import { CurrencyInputs } from '~/components/form/gameInputs/CurrencyInputs';
 import { EvilInputs } from '~/components/form/gameInputs/EvilInputs';
-import { ExpertPathInput } from '~/components/form/gameInputs/ExpertPathInput';
 import { FortuneFateInputs } from '~/components/form/gameInputs/FortuneFateInputs';
 import { HealthInputs } from '~/components/form/gameInputs/HealthInputs';
 import { HistoryInputs } from '~/components/form/gameInputs/HistoryInputs';
-import { MasterPathInput } from '~/components/form/gameInputs/MasterPathInput';
-import { NovicePathInput } from '~/components/form/gameInputs/NovicePathInput';
-import { PerceptionInput } from '~/components/form/gameInputs/PerceptionInput';
 import { PhysicalTraitsInputs } from '~/components/form/gameInputs/PhysicalTraitsInputs';
 import { WeaponInput } from '~/components/form/gameInputs/WeaponInput';
 import { NumberInput } from '~/components/form/NumberInput';
-import { SelectInput } from '~/components/form/SelectInput';
-import { TextAreaInput } from '~/components/form/TextAreaInput';
 import { TextInput } from '~/components/form/TextInput';
 import { Layout } from '~/components/meta/Layout';
 import { FIELD_NAMES } from '~/constants/form';
-import { ANCESTRIES, ATTRIBUTES } from '~/constants/game';
+import { ATTRIBUTES } from '~/constants/game';
 
 const Form = styled(FormComponent)`
   max-width: ${({ theme }) => theme.breakpointValues.lg}px;
