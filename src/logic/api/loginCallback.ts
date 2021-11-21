c09c89f65e7fddd9ae87f8240429f491bfc1fa0c
Troy Chryssos <troy@codecademy.com>
2021-11-20 19:17:36 -0800

    login query to create or find users

--- src/logic/api/loginCallback.ts
+++ src/logic/api/loginCallback.ts
@@ -1,11 +1,37 @@
 import { Session } from '@auth0/nextjs-auth0';
+import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export const loginCallback = (
-  req: NextApiRequest,
-  res: NextApiResponse,
+const prisma = new PrismaClient();
+
+export const loginCallback = async (
+  _req: NextApiRequest,
+  _res: NextApiResponse,
   session: Session
 ) => {
-  const test = '';
+  const { user } = session;
+  let dbUser = await prisma.user.findUnique({
+    where: {
+      authId: user.sub,
+    },
+  });
+
+  if (!dbUser) {
+    const data = {
+      createdOn: new Date(),
+      lastModifiedOn: new Date(),
+      authId: user.sub,
+      role: 'player',
+    };
+    await prisma.user.create({ data });
+    dbUser = await prisma.user.findUnique({
+      where: {
+        authId: user.sub,
+      },
+    });
+  }
+
+  // eslint-disable-next-line no-param-reassign
+  session.user = { ...session.user, ...dbUser };
   return session;
 };
