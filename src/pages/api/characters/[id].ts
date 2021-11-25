5e03b28341d94bd5ec37f3f141d75b218721004c
Troy Chryssos <troy@codecademy.com>
2021-11-25 09:30:06 -0800

    patching characters works

--- src/pages/api/characters/[id].ts
+++ src/pages/api/characters/[id].ts
@@ -1,5 +1,8 @@
+import { withApiAuthRequired } from '@auth0/nextjs-auth0';
 import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
+
+import { CharacterSaveData } from '~/typings/characters';
 
 const prisma = new PrismaClient();
 
@@ -19,4 +22,38 @@ const getCharacter = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 };
 
-export default getCharacter;
+const patchCharacter = withApiAuthRequired(
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+      const { id } = req.query;
+      const body: CharacterSaveData = await JSON.parse(req.body);
+
+      const character = await prisma.character.update({
+        where: {
+          id: parseInt(id as string, 10),
+        },
+        data: {
+          name: body.name,
+          characterCode: body.characterCode,
+          lastModifiedOn: new Date(),
+        },
+      });
+
+      res.status(200).json(character);
+    } catch (e) {
+      res.status(500).json({ error: (e as Error).message });
+    }
+  }
+);
+
+const handleRequest = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+
+  if (method === 'PATCH') {
+    await patchCharacter(req, res);
+  } else {
+    await getCharacter(req, res);
+  }
+};
+
+export default handleRequest;
