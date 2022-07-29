9920c3c8c58384a076956f80106512a5de637225
Troy Chryssos <troy@codecademy.com>
2022-07-29 18:19:20 -0400

    selecting a rulebook as part of the rulebooks flow

--- src/components/admin/Rulebooks.tsx
+++ src/components/admin/Rulebooks.tsx
@@ -1,12 +1,15 @@
 import styled from '@emotion/styled';
 import { rulebook } from '@prisma/client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useFormContext } from 'react-hook-form';
 
+import { NEW_ID } from '~/constants/routing/shared';
 import { fetchRulebook } from '~/logic/api/client/fetchRulebook';
 
 import { TextButton } from '../buttons/TextButton';
 import { Form, FormBox } from '../form/Form';
 import { FormSection } from '../form/FormSection';
+import { SelectInput } from '../form/SelectInput';
 import { TextAreaInput } from '../form/TextAreaInput';
 import { TextInput } from '../form/TextInput';
 
@@ -18,28 +21,77 @@ const defaultRulebook: NewRulebook = {
   description: '',
 };
 
+interface RulebookSelectProps {
+  rulebooks: rulebook[];
+  setActiveRulebook: (rb: rulebook | NewRulebook) => void;
+  activeRulebook: rulebook | NewRulebook;
+}
+
+const RulebookSelect: React.FC<RulebookSelectProps> = ({
+  rulebooks,
+  setActiveRulebook,
+  activeRulebook,
+}) => {
+  const { reset } = useFormContext();
+
+  useEffect(() => {
+    reset(activeRulebook);
+  }, [reset, activeRulebook]);
+
+  const onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    if (e.target.value === NEW_ID) {
+      setActiveRulebook(defaultRulebook);
+    }
+    const rb = rulebooks.find(
+      (book) => book.id === parseInt(e.target.value, 10)
+    );
+    if (rb) {
+      setActiveRulebook(rb);
+    }
+  };
+
+  return (
+    <SelectInput
+      label="Rulebook"
+      options={[
+        { label: 'New Rulebook', value: NEW_ID },
+        ...rulebooks.map((rb) => ({
+          value: String(rb.id),
+          label: rb.fullName,
+        })),
+      ]}
+      onChange={onChange}
+    />
+  );
+};
+
 const RulebookSection = styled(FormSection)`
   width: 100%;
   height: unset;
 `;
 
 export const Rulebooks: React.FC = () => {
-  const [activeRulebook, setActiveRulebook] =
-    useState<Partial<rulebook>>(defaultRulebook);
+  const [rulebooks, setRulebooks] = useState<rulebook[]>([]);
+  const [activeRulebook, setActiveRulebook] = useState<rulebook | NewRulebook>(
+    defaultRulebook
+  );
 
   const onSubmit = async (values: NewRulebook | rulebook) => {
     let rulebookBody;
 
-    if (!activeRulebook.id) {
+    if ((activeRulebook as rulebook).id) {
       rulebookBody = {
-        method: 'POST' as const,
-        rulebook: values as NewRulebook,
+        method: 'PATCH' as const,
+        rulebook: {
+          ...activeRulebook,
+          ...values,
+        } as rulebook,
+        id: (activeRulebook as rulebook).id,
       };
     } else {
       rulebookBody = {
-        method: 'PATCH' as const,
-        rulebook: values as rulebook,
-        id: activeRulebook.id,
+        method: 'POST' as const,
+        rulebook: values as NewRulebook,
       };
     }
     await fetchRulebook(rulebookBody);
@@ -52,6 +104,11 @@ export const Rulebooks: React.FC = () => {
       onSubmit={onSubmit}
     >
       <RulebookSection title="Edit Rulebooks">
+        <RulebookSelect
+          activeRulebook={activeRulebook}
+          rulebooks={rulebooks}
+          setActiveRulebook={setActiveRulebook}
+        />
         <FormBox>
           <TextInput<NewRulebook> label="Name" name="fullName" />
           <TextInput<NewRulebook> label="Abbreviation" name="name" />
