2b04c502331922e8eb3c14f010a2983da30f7441
Troy Chryssos <troy@codecademy.com>
2022-07-31 15:12:16 -0400

    handle request for users id

--- src/pages/api/users/[id]/index.ts
+++ src/pages/api/users/[id]/index.ts
@@ -1,8 +1,9 @@
-import { NextApiHandler } from 'next';
+import { user } from '@prisma/client';
+import { NextApiHandler, NextApiRequest } from 'next';
 
 import { prisma } from '~/logic/utils/prisma';
 
-const fetchProfile: NextApiHandler = async (req, res) => {
+const getUser: NextApiHandler = async (req, res) => {
   try {
     const {
       query: { id },
@@ -19,4 +20,38 @@ const fetchProfile: NextApiHandler = async (req, res) => {
   }
 };
 
-export default fetchProfile;
+export type PatchUserData = Pick<user, 'email' | 'role'>;
+
+const patchUser: NextApiHandler = async (req, res) => {
+  try {
+    const { id } = req.query;
+    const body: PatchUserData = await JSON.parse(req.body);
+
+    const updateUser = await prisma.user.update({
+      where: {
+        id: parseInt(id as string, 10),
+      },
+      data: {
+        email: body.email,
+        role: body.role,
+        lastModifiedOn: new Date(),
+      },
+    });
+
+    res.status(200).json(updateUser);
+  } catch (e) {
+    res.status(500).json({ error: (e as Error).message });
+  }
+};
+
+const handleRequest: NextApiHandler = async (req, res) => {
+  const { method } = req;
+
+  if (method === 'PATCH') {
+    await patchUser(req, res);
+  } else {
+    await getUser(req, res);
+  }
+};
+
+export default handleRequest;
