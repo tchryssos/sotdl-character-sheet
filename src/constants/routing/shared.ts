4a314e6b646d3a8810218522cd87da9c75c7c1d9
Troy Chryssos <troy@codecademy.com>
2022-07-31 17:10:14 -0400

    new character page rulebook rendering

--- src/constants/routing/shared.ts
+++ src/constants/routing/shared.ts
@@ -5,8 +5,8 @@ export const RULEBOOKS_PATH = 'rulebooks';
 
 export const NEW_ID = 'new';
 
-export const createCharacterRoute = (id: number | string) =>
-  `/${CHARACTERS_PATH}/${id}`;
+export const createCharacterRoute = (id: number | string, rulebook: string) =>
+  `/${CHARACTERS_PATH}/${rulebook}/${id}`;
 export const createUsersRoute = (id: number | string) => `/${USERS_PATH}/${id}`;
 export const createRulebookRoute = (id: number | string) =>
   `/${RULEBOOKS_PATH}/${id}`;
--- src/pages/characters/new.tsx
+++ src/pages/characters/new.tsx
@@ -1,7 +1,29 @@
-import { useContext, useEffect } from 'react';
+import styled from '@emotion/styled';
+import { rulebook } from '@prisma/client';
+import { useContext, useEffect, useState } from 'react';
 
+import { FlexBox } from '~/components/box/FlexBox';
+import { Link } from '~/components/Link';
+import { LoadingPageSpinner } from '~/components/LoadingSpinner';
 import { Layout } from '~/components/meta/Layout';
+import { Title } from '~/components/typography/Title';
+import { ALL_RULEBOOKS_API_PATH } from '~/constants/routing/api';
+import { createCharacterRoute, NEW_ID } from '~/constants/routing/shared';
 import { NavContext } from '~/logic/contexts/navContext';
+
+const RulebookList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing[16]};
+`;
+
+const RulebookBox = styled(FlexBox)(({ theme }) => ({
+  border: `${theme.border.borderWidth[1]} solid ${theme.colors.text}`,
+  padding: theme.spacing[24],
+  [theme.breakpoints.md]: {
+    padding: theme.spacing[40],
+  },
+}));
 
 const NewCharacterNav: React.FC = () => {
   const { setNavTitle } = useContext(NavContext);
@@ -13,15 +35,66 @@ const NewCharacterNav: React.FC = () => {
   return null;
 };
 
+interface SelectRulebookProps {
+  rulebooks: rulebook[];
+}
+
+const SelectRulebook: React.FC<SelectRulebookProps> = ({ rulebooks }) => (
+  <RulebookList>
+    {rulebooks.map((rb) => (
+      <li key={rb.id}>
+        <Link href={createCharacterRoute(NEW_ID, rb.name)}>
+          <RulebookBox>
+            <Title>{rb.fullName}</Title>
+          </RulebookBox>
+        </Link>
+      </li>
+    ))}
+  </RulebookList>
+);
+
+const emptyRbs: rulebook[] = [];
+
 const NewCharacterPage: React.FC = () => {
-  const test = '';
+  const [isLoading, setIsLoading] = useState(false);
+  const [rulebooks, setRulebooks] = useState<rulebook[]>([]);
+  const [hasError, setHasError] = useState(false);
+
+  const getRulebooks = async () => {
+    setIsLoading(true);
+    const resp = await fetch(ALL_RULEBOOKS_API_PATH, {
+      method: 'GET',
+    });
+
+    if (resp.status >= 200 && resp.status <= 300) {
+      const respData = await resp.json();
+      setRulebooks(respData);
+      setHasError(false);
+    } else {
+      setRulebooks(emptyRbs);
+      setHasError(true);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getRulebooks();
+  }, []);
+
   return (
     <Layout
       meta="Select a rulebook for your new character"
       title="New Character"
     >
       <NewCharacterNav />
-      <span>new</span>
+      {isLoading ? (
+        <LoadingPageSpinner
+          title="Loading rulebooks"
+          titleId="new-char-rulebook-loading"
+        />
+      ) : (
+        <SelectRulebook rulebooks={rulebooks} />
+      )}
     </Layout>
   );
 };
