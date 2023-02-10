27936ca275ce1db8a4261581c2eb12600da9f3b6
Troy Chryssos <troy@codecademy.com>
2023-02-10 11:23:22 -0500

    fixing prune

--- src/logic/tests/mockNotifications.ts
+++ src/logic/tests/mockNotifications.ts
@@ -1,69 +1,69 @@
 import sortBy from 'lodash.sortby';
 
-import { FREE_USER_CHARACTER_LIMIT_MESSAGE } from '~/constants/notifications/errors';
+import { ERRORS } from '~/constants/notifications/errors';
 
 export const MOCK_NOTIFICATIONS = sortBy(
   [
     {
       id: '1',
-      title: 'No more free character slots',
-      message: FREE_USER_CHARACTER_LIMIT_MESSAGE,
+      title: ERRORS['Free user character limit'].title,
+      message: ERRORS['Free user character limit'].message,
       type: 'error',
       createdOn: new Date('2022-01-01'),
     },
     {
       id: '2',
-      title: 'No more free character slots',
-      message: FREE_USER_CHARACTER_LIMIT_MESSAGE,
+      title: ERRORS['Free user character limit'].title,
+      message: ERRORS['Free user character limit'].message,
       type: 'info',
       createdOn: new Date('2022-09-01'),
     },
     {
       id: '3',
-      title: 'No more free character slots',
-      message: FREE_USER_CHARACTER_LIMIT_MESSAGE,
+      title: ERRORS['Free user character limit'].title,
+      message: ERRORS['Free user character limit'].message,
       type: 'success',
       createdOn: new Date('2023-02-07'),
     },
     {
       id: '4',
-      title: 'No more free character slots',
-      message: FREE_USER_CHARACTER_LIMIT_MESSAGE,
+      title: ERRORS['Free user character limit'].title,
+      message: ERRORS['Free user character limit'].message,
       type: 'error',
       createdOn: new Date('2023-01-08'),
     },
     {
       id: '5',
-      title: 'No more free character slots',
-      message: FREE_USER_CHARACTER_LIMIT_MESSAGE,
+      title: ERRORS['Free user character limit'].title,
+      message: ERRORS['Free user character limit'].message,
       type: 'info',
       createdOn: new Date('2023-02-06'),
     },
     {
       id: '6',
-      title: 'No more free character slots',
-      message: FREE_USER_CHARACTER_LIMIT_MESSAGE,
+      title: ERRORS['Free user character limit'].title,
+      message: ERRORS['Free user character limit'].message,
       type: 'success',
       createdOn: new Date('2021-08-01'),
     },
     {
       id: '7',
-      title: 'No more free character slots',
-      message: FREE_USER_CHARACTER_LIMIT_MESSAGE,
+      title: ERRORS['Free user character limit'].title,
+      message: ERRORS['Free user character limit'].message,
       type: 'error',
       createdOn: new Date('2023-02-01'),
     },
     {
       id: '8',
-      title: 'No more free character slots',
-      message: FREE_USER_CHARACTER_LIMIT_MESSAGE,
+      title: ERRORS['Free user character limit'].title,
+      message: ERRORS['Free user character limit'].message,
       type: 'info',
       createdOn: new Date('2022-12-12'),
     },
     {
       id: '9',
-      title: 'No more free character slots',
-      message: FREE_USER_CHARACTER_LIMIT_MESSAGE,
+      title: ERRORS['Free user character limit'].title,
+      message: ERRORS['Free user character limit'].message,
       type: 'success',
       createdOn: new Date(),
     },
--- src/typings/util.d.ts
+++ src/typings/util.d.ts
@@ -12,14 +12,14 @@ type KeyOfListField<T extends Record<string, unknown>> = KeysOfUnion<
   ArrayElementType<ValuesOf<ListFieldRecord<T>>>
 >;
 
-type NonListFieldRecord<T extends Record<string, unknown>> = {
-  [K in keyof T as T[K] extends string ? K : never]: T[K];
-};
+// type NonListFieldRecord<T extends Record<string, unknown>> = {
+//   [K in keyof T as T[K] extends string ? K : never]: T[K];
+// };
 
 type ListFieldRecord<T extends Record<string, unknown>> = {
   [K in keyof T as T[K] extends unknown[] ? K : never]: T[K];
 };
 
-type BooleanFieldsRecord<T extends Record<string, unknown>> = {
-  [K in keyof T as T[K] extends boolean ? K : never]: T[K];
-};
+// type BooleanFieldsRecord<T extends Record<string, unknown>> = {
+//   [K in keyof T as T[K] extends boolean ? K : never]: T[K];
+// };
