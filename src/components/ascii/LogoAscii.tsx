c24946c7169e552f5a9a90249cb411fe31b6c80b
Troy Chryssos <troychryssos@gmail.com>
2023-02-20 11:20:34 -0500

    unify ascii font size logic into util

--- src/components/ascii/LogoAscii.tsx
+++ src/components/ascii/LogoAscii.tsx
@@ -1,39 +1,19 @@
 import { SCIMITAR } from '~/constants/ascii';
+import { getAsciiFontSize } from '~/logic/utils/ascii';
 
 import { Ascii } from './Ascii';
+import { AsciiSize } from './types';
 
 interface LogoAsciiProps {
-  size?: 'xs' | 'sm' | 'md';
+  size?: AsciiSize;
   className?: string;
 }
 
 export function LogoAscii({ size = 'md', className }: LogoAsciiProps) {
-  /**
-   * This fontSize doesn't use rems for two reasons:
-   * 1) Practically speaking, we don't want this font size to change if people mess with
-   * browser font sizes since it's not actually text.
-   * 2) More bizarrely, Safari seems to set a "min size" for text using non-px units at
-   * ~9px. This makes the logo HUGE. See https://stackoverflow.com/questions/52001765/media-queries-issue-safari-not-scaling-elements-in-rem
-   *
-   * TODO: Maybe this should just be an image?
-   */
-  let fontSize: number;
-  switch (size) {
-    case 'xs':
-      fontSize = 1;
-      break;
-    case 'sm':
-      fontSize = 2;
-      break;
-    default:
-      fontSize = 6;
-      break;
-  }
-
   return (
     <Ascii
       className={className}
-      fontSize={fontSize}
+      fontSize={getAsciiFontSize(size)}
       label="SCIMITAR"
       text={SCIMITAR}
     />
--- src/components/ascii/NotFoundAscii.tsx
+++ src/components/ascii/NotFoundAscii.tsx
@@ -1,33 +1,19 @@
 import { SKULL } from '~/constants/ascii';
+import { getAsciiFontSize } from '~/logic/utils/ascii';
 
 import { Ascii } from './Ascii';
+import { AsciiSize } from './types';
 
 interface LogoAsciiProps {
-  size?: 'xs' | 'sm' | 'md' | 'lg';
+  size?: AsciiSize;
   className?: string;
 }
 
 export function NotFoundAscii({ size = 'lg', className }: LogoAsciiProps) {
-  let fontSize: number;
-  switch (size) {
-    case 'xs':
-      fontSize = 1;
-      break;
-    case 'sm':
-      fontSize = 2;
-      break;
-    case 'lg':
-      fontSize = 8;
-      break;
-    default:
-      fontSize = 6;
-      break;
-  }
-
   return (
     <Ascii
       className={className}
-      fontSize={fontSize}
+      fontSize={getAsciiFontSize(size)}
       label="NOT FOUND"
       text={SKULL}
     />
