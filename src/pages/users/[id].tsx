4edf10c2b0f0405fa276e3ad22ddb4e697a450f6
Troy Chryssos <troychryssos@gmail.com>
2023-03-02 22:38:48 -0500

    testing out profile meta top section

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,14 +1,25 @@
+import styled from '@emotion/styled';
+import { format } from 'date-fns';
 import { GetServerSideProps } from 'next';
 
+import { FlexBox } from '~/components/box/FlexBox';
 import { GridBox } from '~/components/box/GridBox';
 import { Layout } from '~/components/meta/Layout';
 import { ProfileNav } from '~/components/nav/ProfileNav';
 import { CharactersSection } from '~/components/profile/CharactersSection';
+import { Text } from '~/components/Text';
+import { SHORT_DATE_FORMAT } from '~/constants/dates';
 import { prisma } from '~/logic/utils/prisma';
 import { StrictCharacter } from '~/typings/characters';
 import { StrictUser } from '~/typings/user';
 
 import FourOhFour from '../404';
+
+const DummyImage = styled.div`
+  height: 100px;
+  width: 100px;
+  background-color: ${({ theme }) => theme.colors.accentHeavy};
+`;
 
 interface ProfilePageProps {
   userMeta?: Pick<StrictUser, 'createdOn' | 'isPaid'>;
@@ -23,7 +34,18 @@ function ProfilePage({ userMeta, userCharacters }: ProfilePageProps) {
   return (
     <Layout meta="rpg sheet profile" title="Profile">
       <ProfileNav />
-      <GridBox columns={1} width="100%">
+      <GridBox columns={1} gap={16} width="100%">
+        <FlexBox alignItems="center" gap={16}>
+          <DummyImage />
+          <FlexBox flexDirection="column" gap={8}>
+            <Text as="h1" variant="title">
+              Doggy Man
+            </Text>
+            <Text as="p" variant="body-xs">
+              Joined {format(userMeta.createdOn, SHORT_DATE_FORMAT)}
+            </Text>
+          </FlexBox>
+        </FlexBox>
         <CharactersSection characters={userCharacters} />
       </GridBox>
     </Layout>
