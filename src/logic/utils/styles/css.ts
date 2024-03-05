d1016b5eb7191964e6682b2c7721d81db547943f
Troy Chryssos <troychryssos@gmail.com>
2024-03-05 18:28:44 -0500

    fixing breakpoint style specifity bug

--- src/logic/utils/styles/css.ts
+++ src/logic/utils/styles/css.ts
@@ -130,84 +130,112 @@ const handleCustomCssProps = ({
 type CssPropObj = Partial<CSSObject & AllowedCustomCssSpacingProps>;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const filterCssProps = (props: Record<string, any>, theme: Theme) =>
-  Object.keys(props).reduce((propObj: CssPropObj, currPropKey) => {
-    // Create a copy of the propObj so we don't mutate it
-    let nextPropObj = { ...propObj } as CssPropObj;
-    const propValue = props[currPropKey];
-    if (propValue) {
-      // Check if the current prop is a style-by-breakpoint object
-      if (isStyleByBreakpointObj(propValue)) {
-        const breakpoints = Object.keys(propValue) as StyleByBreakpointKeys[];
-        // For each breakpoint...
-        breakpoints.forEach((breakpoint) => {
-          // ... if that breakpoint is "base", treat it like a regular prop ...
-          if (breakpoint === 'base') {
-            nextPropObj = {
-              ...nextPropObj,
-              ...filterCssProps({ [currPropKey]: propValue.base }, theme),
-            };
-          } else {
-            // ...otherwise, get the mediaQuery from the theme and nest the prop
-            // under that breakpoint
-            const value = propValue[breakpoint];
-            if (value !== undefined) {
+export const filterCssProps = (props: Record<string, any>, theme: Theme) => {
+  let cssPropObj = Object.keys(props).reduce(
+    (propObj: CssPropObj, currPropKey) => {
+      // Create a copy of the propObj so we don't mutate it
+      let nextPropObj = { ...propObj } as CssPropObj;
+      const propValue = props[currPropKey];
+      if (propValue) {
+        // Check if the current prop is a style-by-breakpoint object
+        if (isStyleByBreakpointObj(propValue)) {
+          const breakpoints = Object.keys(propValue) as StyleByBreakpointKeys[];
+          // For each breakpoint...
+          breakpoints.forEach((breakpoint) => {
+            // ... if that breakpoint is "base", treat it like a regular prop ...
+            if (breakpoint === 'base') {
               nextPropObj = {
                 ...nextPropObj,
-                [theme.breakpoints[breakpoint]]: {
-                  ...((nextPropObj[
-                    theme.breakpoints[breakpoint]
-                  ] as CssPropObj) || {}),
-                  ...filterCssProps({ [currPropKey]: value }, theme),
-                },
+                ...filterCssProps({ [currPropKey]: propValue.base }, theme),
               };
+            } else {
+              // ...otherwise, get the mediaQuery from the theme and nest the prop
+              // under that breakpoint
+              const value = propValue[breakpoint];
+              if (value !== undefined) {
+                nextPropObj = {
+                  ...nextPropObj,
+                  [theme.breakpoints[breakpoint]]: {
+                    ...((nextPropObj[
+                      theme.breakpoints[breakpoint]
+                    ] as CssPropObj) || {}),
+                    ...filterCssProps({ [currPropKey]: value }, theme),
+                  },
+                };
+              }
             }
-          }
-        });
-        // This else always true during a recursive call of this fn due to breakpoints
-      } else {
-        // Check if the current prop is a custom CSS prop
-        // for ex. paddingX, paddingY, etc.
-        const usesCustomCss =
-          Object.keys(customCssMapping).includes(currPropKey);
-
-        if (usesCustomCss) {
-          // If it is, handle it using a helper function
-          const customProps = handleCustomCssProps({
-            currPropKey: currPropKey as keyof AllowedCustomCssProps,
-            theme,
-            propValue,
           });
-          nextPropObj = { ...nextPropObj, ...customProps };
+          // This else always true during a recursive call of this fn due to breakpoints
         } else {
-          // Check if it is a custom theme prop
-          // aka any prop for which we expect to match a defined theme value
-          const usesCustomTheme = Object.keys(CUSTOM_THEME_CSS_PROPS).includes(
-            currPropKey
-          );
-          if (usesCustomTheme) {
+          // Check if the current prop is a custom CSS prop
+          // for ex. paddingX, paddingY, etc.
+          const usesCustomCss =
+            Object.keys(customCssMapping).includes(currPropKey);
+
+          if (usesCustomCss) {
             // If it is, handle it using a helper function
-            nextPropObj[currPropKey] = handleThemedCssProps({
-              currPropKey: currPropKey as keyof typeof CUSTOM_THEME_CSS_PROPS,
+            const customProps = handleCustomCssProps({
+              currPropKey: currPropKey as keyof AllowedCustomCssProps,
               theme,
               propValue,
             });
-          } else if (
-            // If it isn't, check if it is a valid CSS prop
-            ALL_ALLOWED_CSS_PROPS.includes(
-              currPropKey as (typeof ALL_ALLOWED_CSS_PROPS)[number]
-            )
-          ) {
-            // If it is, add it to the propObj
-            nextPropObj[currPropKey] = propValue;
+            nextPropObj = { ...nextPropObj, ...customProps };
+          } else {
+            // Check if it is a custom theme prop
+            // aka any prop for which we expect to match a defined theme value
+            const usesCustomTheme = Object.keys(
+              CUSTOM_THEME_CSS_PROPS
+            ).includes(currPropKey);
+            if (usesCustomTheme) {
+              // If it is, handle it using a helper function
+              nextPropObj[currPropKey] = handleThemedCssProps({
+                currPropKey: currPropKey as keyof typeof CUSTOM_THEME_CSS_PROPS,
+                theme,
+                propValue,
+              });
+            } else if (
+              // If it isn't, check if it is a valid CSS prop
+              ALL_ALLOWED_CSS_PROPS.includes(
+                currPropKey as (typeof ALL_ALLOWED_CSS_PROPS)[number]
+              )
+            ) {
+              // If it is, add it to the propObj
+              nextPropObj[currPropKey] = propValue;
+            }
           }
         }
       }
-    }
 
-    // Return the propObj
-    return nextPropObj;
-  }, {} as CssPropObj);
+      // Return the propObj
+      return nextPropObj;
+    },
+    {} as CssPropObj
+  );
+
+  /**
+   * Because of how emotion handles media query order
+   * we were getting unexpected results when multiple css props
+   * with different media queries were all getting parsed.
+   *
+   * This was leading to styles from smaller breakpoints getting
+   * greater specificity than larger breakpoints.
+   *
+   * This stupid fn reorders the media queries so that the larger queries
+   * have greater specificity, and therefore, properly display their styles
+   */
+  Object.values(theme.breakpoints).forEach((breakpoint) => {
+    const breakpointStyles = cssPropObj[breakpoint];
+    if (breakpointStyles) {
+      delete cssPropObj[breakpoint];
+      cssPropObj = {
+        ...cssPropObj,
+        [breakpoint]: breakpointStyles,
+      };
+    }
+  });
+
+  return cssPropObj;
+};
 
 export const makeCssPropStyles = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
